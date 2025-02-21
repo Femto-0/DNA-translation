c39acb01ec88e4f2b80964e7eb4229af372f1aeb
Ritesh Shah <arits.king74@gmail.com>
2025-02-20 23:54:12 -0600

    Provided some context about project

--- src/Translation.java
+++ src/Translation.java
@@ -1,2 +1,13 @@
+/*
+The idea behind making this project is to use my knowledge of protein synthesis from m-RNA using r-RNA and t-RNA.
+The program will generate result based on given m-RNA sequence
+The program might expand to: DNA-> RNA -> m-RNA -> Protein but for now it's just m-RNA -> Protein
+
+Author: Ritesh Shah
+Date: Feb 19, 2025
+ */
+
+
 public class Translation {
+
 }
