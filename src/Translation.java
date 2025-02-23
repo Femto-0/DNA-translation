6f391ecc4e1ca91061f77884dbed39e2eb66e66c
Ritesh Shah <arits.king74@gmail.com>
2025-02-23 01:02:29 -0600

    Added a method to convert a full sequence to chain of Amino Acid and also method to check start and end codon

--- src/Translation.java
+++ src/Translation.java
@@ -14,6 +14,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class Translation {
 
@@ -46,12 +47,51 @@ public class Translation {
          }
     }
 
+    public int findStartingCodon(String sequence){
+        for(int i=0; i<sequence.length()-1; i++){
+            if(sequence.substring(i, i+3).equals("AUG")){ //begin--> inclusive end-->exclusive
+            return i;
+            }
+        }
+        return -1;
+    }
+    public int findStopCodon(String sequence){
+        String correctFormatForSequence= sequence+" "; //had to do this because the way substrings are formed in Java don't support how I want this method to act.
+        for(int i = correctFormatForSequence.length()-1; true; i--){
+            if(correctFormatForSequence.substring(i-2, i+1).equals("UAA")){ //begin--> inclusive end-->exclusive
+                return i-2;
+            }else{
+                return -1;
+            }
+        }
+    }
+
+
+    public ArrayList<String> aminoAcidChain(String sequence) throws IOException {
+        ArrayList<String> protein = new ArrayList<>();
+        int startFrom=findStartingCodon(sequence);
+        int stopAt=findStopCodon(sequence);
+        if(startFrom!=-1 && stopAt!=-1){
+            for(int i=startFrom; i<=stopAt-3; i+=3){
+                String codon= sequence.substring(i, i+3);
+                String aminoAcid=findAminoAcid(codon);
+                protein.add(aminoAcid);
+            }
+            protein.add("STOP");
+        }else{
+            System.out.println("Start or end codon not found");
+            System.exit(0);
+        }
+        return protein;
+    }
+
 
 
 
     public static void main(String[] args) throws IOException {
         Translation t = new Translation();
-        System.out.println(t.findAminoAcid("AUG"));
+        String sequence= "AUGUUAUA";
+        System.out.println(t.aminoAcidChain(sequence));
     }
 
 }
